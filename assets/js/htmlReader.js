94c5157a5abc0df2006da08171eee01961c8f270
David <david@davidcraft.de>
2023-04-22 14:44:45 +0000

    Debug
    Signed-off-by: GitHub <noreply@github.com>

--- assets/js/htmlReader.js
+++ assets/js/htmlReader.js
@@ -1,5 +1,6 @@
 function readHTML (url, div) {
     fetch(url)
+        .then(response => console.log(response))
         .then(response => response.text())
         .then(text => div.innerHTML = text)
 }
